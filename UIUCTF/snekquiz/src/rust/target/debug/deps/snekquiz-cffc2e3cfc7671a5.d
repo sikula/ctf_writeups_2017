7237b0030c5f17ce130788253f1962676d5f4093
Peter Sikula <peter.sikula@saigar.io>
2017-04-30 22:35:06 +0000

    added UICTF writeup

--- UIUCTF/snekquiz/src/rust/target/debug/deps/snekquiz-cffc2e3cfc7671a5.d
+++
@@ -1,5 +0,0 @@
-/home/sikula/Vagrant/UbuntuXenial/CTFS/UIUCTF/exploits/snekquiz/target/debug/deps/snekquiz-cffc2e3cfc7671a5: src/main.rs
-
-/home/sikula/Vagrant/UbuntuXenial/CTFS/UIUCTF/exploits/snekquiz/target/debug/deps/snekquiz-cffc2e3cfc7671a5.d: src/main.rs
-
-src/main.rs:
--- UIUCTF/snekquiz/src/rust/target/debug/snekquiz.d
+++
@@ -1,1 +0,0 @@
-/home/sikula/Programs/git_stuff/ctf_writeups_2017/UIUCTF/snekquiz/src/rust/target/debug/snekquiz: /home/sikula/Programs/git_stuff/ctf_writeups_2017/UIUCTF/snekquiz/src/rust/src/main.rs
